987dde887941dbf3d75f75eb0df02f36b87cf894
aahic <62832167+adiahic@users.noreply.github.com>
2020-04-28 16:30:12 +0200

    prvo, prva finkcija

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,35 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+FlatButton dugme({
+  String clan,
+  String zvuk,
+}) {
+  return FlatButton(
+    child: Text(
+      clan,
+      style: TextStyle(fontSize: 55.0, color: Colors.lightBlue),
+      textAlign: TextAlign.center,
+    ),
+    onPressed: () {
+      final player = AudioCache();
+      player.play(zvuk);
+      print('klik ok');
+    },
+  );
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -41,10 +66,7 @@ class MyApp extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text(
-                    'Babo',
-                    textAlign: TextAlign.center,
-                  ),
+                  dugme(clan: 'tata', zvuk: 'Babo.m4a'),
                   SizedBox(
                     width: 25.0,
                   ),
@@ -66,7 +88,7 @@ class MyApp extends StatelessWidget {
                   SizedBox(
                     width: 25.0,
                   ),
-                  Text('Mama'),
+                  dugme(clan: 'mama', zvuk: 'Mama.m4a'),
                 ],
               ),
             ),
@@ -74,7 +96,7 @@ class MyApp extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text('Dino'),
+                  dugme(clan: 'Dino', zvuk: 'Dino.m4a'),
                   SizedBox(
                     width: 25.0,
                   ),
@@ -96,7 +118,7 @@ class MyApp extends StatelessWidget {
                   SizedBox(
                     width: 25.0,
                   ),
-                  Text('Kan'),
+                  dugme(clan: 'Kanano', zvuk: 'Kan.m4a'),
                 ],
               ),
             ),
